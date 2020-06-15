78f1776037b22f97f4068cffe74aa1d60c02a343
HishamAl-Naji <hishamnaji1010@gmail.com>
2020-06-15 17:01:43 +0300

    rearranging the code

--- JS/app.js
+++ JS/app.js
@@ -33,16 +33,10 @@ City.prototype.getCookies = function (numCos, AvgCookie) {
     this.numCookies = generatCookies(numCos, AvgCookie);
 };
 
+
+// ------------------------------ Reandering 
+
 var table = document.createElement('table');
-var tr = document.createElement('tr')
-table.appendChild(tr)
-for (var i = 0; i < Time.length; i++) {
-    var th = document.createElement('th');
-    tr.appendChild(th);
-    th.textContent = Time[i];
-
-}
-
 
 City.prototype.render = function (location, title) {
     var mainTag = document.getElementsByTagName('main')[0];
@@ -97,7 +91,6 @@ City.prototype.render = function (location, title) {
 
 }
 
-
 // To generate a random number of costumers
 function generateRandomCostumerNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -108,9 +101,52 @@ function generatCookies(randomCust, AvgCookie) {
     return Math.floor(randomCust * AvgCookie);
 }
 
+// ------------------------------ header raw Function :
 
-// ------------------------------ All shops
+function headerRaw() {
+    var table = document.createElement('table');
+    var tr = document.createElement('tr')
+    table.appendChild(tr)
+    for (var i = 0; i < Time.length; i++) {
+        var th = document.createElement('th');
+        tr.appendChild(th);
+        th.textContent = Time[i];
 
+    }
+}
+
+// ------------------------------ Footer Raw Function :
+
+function footerRaw() {
+    var tr = document.createElement('tr')
+    table.appendChild(tr)
+    var td = document.createElement('td');
+    tr.appendChild(td);
+    td.textContent = "Total";
+
+    var allCookies = 0;
+    for (var i = 0; i < 14; i++) {
+        var sumHourlyCookies = 0;
+        var td = document.createElement('td');
+        for (var z = 0; z < 5; z++) {
+            sumHourlyCookies = sumHourlyCookies + Cities[z].hourlyCookies[i]
+        }
+        allCookies = allCookies + sumHourlyCookies
+        td.textContent = sumHourlyCookies + " Cookies";
+        tr.appendChild(td);
+    }
+    var td = document.createElement('td');
+    tr.appendChild(td);
+    td.textContent = allCookies + " cookies";
+
+}
+
+
+
+
+// ------------------------------ All shops Renderings :
+
+headerRaw();
 
 var Seattle = new City(65, 23, 6.3);
 var Tikyo = new City(24, 3, 1.2);
@@ -128,26 +164,5 @@ for (var i = 0; i < Cities.length; i++) {
 
 }
 
-// ------------------------------ Daily Location Total raw
+footerRaw();
 
-
-var tr = document.createElement('tr')
-table.appendChild(tr)
-var td = document.createElement('td');
-tr.appendChild(td);
-td.textContent = "Total";
-
-var allCookies = 0 ;
-for (var i = 0; i < 14; i++) {
-    var sumHourlyCookies = 0;
-    var td = document.createElement('td');
-    for (var z = 0; z < 5; z++) {
-        sumHourlyCookies = sumHourlyCookies + Cities[z].hourlyCookies[i]
-    }
-    allCookies = allCookies + sumHourlyCookies
-    td.textContent = sumHourlyCookies + " Cookies";
-    tr.appendChild(td);
-}
-var td = document.createElement('td');
-tr.appendChild(td);
-td.textContent = allCookies + " cookies";
